1892a4089cd6f0b9bf0b012aa1d537e6f313f009
damxaq <damiancelico@gmail.com>
2021-06-18 21:55:45 +0200

    info if registration password is not same, or email exist

--- src/RegisterModal.js
+++ src/RegisterModal.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "firebase/app";
 
 const RegisterModal = () => {
+  const [showWrongPassword, setShowWrongPassword] = useState(false);
+  const [showEmailExist, setShowEmailExist] = useState(false);
   const registerAccount = (e) => {
+    if (showEmailExist) setShowEmailExist(false);
+    if (showWrongPassword) setShowWrongPassword(false);
     e.preventDefault();
     const email = e.target[0].value;
     const password = e.target[1].value;
     const confPassowrd = e.target[2].value;
 
-    console.log(email, password, confPassowrd);
-
-    // TODO: show error if pass is wrong
+    if (password && password !== confPassowrd) {
+      setShowWrongPassword(true);
+    }
     if (email && password && password === confPassowrd) {
       firebase
         .auth()
@@ -18,6 +22,7 @@ const RegisterModal = () => {
         .catch(function (error) {
           console.log(error.code);
           console.log(error.message);
+          setShowEmailExist(true);
         });
     }
   };
@@ -49,6 +54,9 @@ const RegisterModal = () => {
           required
           placeholder="Confirm Password"
         />
+
+        {showWrongPassword && <div>Password is not matching!</div>}
+        {showEmailExist && <div>Email already exist!</div>}
 
         <button type="submit" className="sign-form-button">
           Sign Up
